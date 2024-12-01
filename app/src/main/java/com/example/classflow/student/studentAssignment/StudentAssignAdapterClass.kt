4933597e9494766b9a479e56047b7a07898e762c
Shristi Gupta <shristigupta@Shristis-MacBook-Air.local>
2024-12-02 02:58:41 +0530

    Student Assignment Screen n Student home

---
+++ app/src/main/java/com/example/classflow/student/studentAssignment/StudentAssignAdapterClass.kt
@@ -0,0 +1,38 @@
+package com.example.classflow.student.studentAssignment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.classflow.databinding.StudentAssignmentCardBinding
+
+class StudentAssignAdapterClass(
+    private val datalist: List<StudentAssignmentDataClass>
+) : RecyclerView.Adapter<StudentAssignAdapterClass.ViewHolder>() {
+
+    // ViewHolder Class
+    class ViewHolder(private val binding: StudentAssignmentCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: StudentAssignmentDataClass) {
+            // Binding data to the views
+            binding.StuAssignmentSubject.text = data.stuAssignSub
+            binding.StuAssignedDate.text = data.stuAssignDate
+            binding.StuDeadline.text = data.stuAssignDeadline
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Inflate the layout for each item
+        val binding = StudentAssignmentCardBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return datalist.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(datalist[position])
+    }
+}
---
+++ app/src/main/java/com/example/classflow/student/studentAssignment/StudentAssignmentDataClass.kt
@@ -0,0 +1,8 @@
+package com.example.classflow.student.studentAssignment
+
+// Define a data class
+class StudentAssignmentDataClass(
+    var stuAssignSub: String,    // Subject of the assignment
+    var stuAssignDate: String,  // Date when the assignment was assigned
+    var stuAssignDeadline: String // Deadline for submitting the assignment
+)
